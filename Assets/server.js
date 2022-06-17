4265f44c848afb50c39919b2be1ef1b6c3f07497
macosta2015 <macosta2015@my.fit.edu>
2022-06-17 15:38:58 -0400

    Adding the inquirer code

--- Assets/server.js
+++ Assets/server.js
@@ -2,8 +2,70 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 
 
-
-myFunction();
 function myFunction(){
     console.log("Hello World");
+    startApplication()
 }
+
+
+function startApplication (){
+    inquirer.prompt([
+        {
+            name: 'initialInquiry',
+            type: 'rawlist',
+            message: 'Welcome to the employee management program. What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'View all employees by manager', 'Add a department', 'Add a role', 'Add an employee', 'Update employee\'s role', 'Update employee\'s manager', 'Remove a department', 'Remove a role', 'Remove an employee', 'View total salary of department', 'Exit program']
+        }
+    ]).then((response) => {
+        switch (response.initialInquiry) {
+            case 'View all departments':
+                viewAllDepartments();    
+                break;
+            case 'View all roles':
+                viewAllRoles();
+                break;
+            case 'View all employees':
+                viewAllEmployees();
+                break;
+            case 'View all employees by manager':
+                viewAllEmployeesByManager();
+            break;
+            case 'Add a department':
+                addADepartment();
+            break;
+            case 'Add a role':
+                addARole();
+            break;
+            case 'Add an employee':
+                addAnEmployee();
+            break;
+            case 'Update employee\'s role':
+                updateEmployeeRole();
+            break;
+            case 'Update employee\'s manager':
+                updateEmployeesManager();
+            break;
+            case 'Remove a department':
+                removeADepartment();
+            break;
+            case 'Remove a role':
+                removeARole();
+            break;
+            case 'Remove an employee':
+                removeAnEmployee();
+            break;
+            case 'View total salary of department':
+                viewDepartmentSalary();
+            break;
+            case 'Exit program':
+                connection.end();
+                console.log('\n You have exited the employee management program. Thanks for using! \n');
+                return;
+            default:
+                break;
+        }
+    })
+}
+
+
+myFunction();
